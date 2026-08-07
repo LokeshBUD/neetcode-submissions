cb37468cb375f2b77e2a7fb9ce96ccfde2abbd8c
Lokesh Budda <54324841+LokeshBUD@users.noreply.github.com>
2026-08-07 01:44:59 -0700

    Add docstring to productExceptSelf method
    Added a docstring to explain the logic behind the productExceptSelf method.

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -1,5 +1,10 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        Basically, the idea here is that prefix product will take everything from the left of that index, multiply it, and store it at that index. 
+        Postfix will take the right value, multiply it all, and store it at that index. That way, we are getting both sides of all the indices, 
+        and that is the final result. 
+        """
         res = [1] * len(nums)
         
         prefix = 1

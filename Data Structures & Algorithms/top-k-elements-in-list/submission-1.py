68f7e222d31bf5e81c01aa72993336d816eb7aac
Lokesh Budda <54324841+LokeshBUD@users.noreply.github.com>
2026-08-07 00:55:41 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        """In this array, we know that the frequency array contains the number value at each frequency index. When we traverse from the end of the array, we are automatically going from maximum frequency, and by appending the result list with each value, we are able to get the K top frequency values. """
+        
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+        
